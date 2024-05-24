4162f333399c4c87fb5d0d7c440068d509a606d7
Rory Kaletsch <rorykaletsch@gmail.com>
2024-05-24 20:09:55 +0200

    Initial commit

---
+++ src/index/index.js
@@ -0,0 +1,19 @@
+document.getElementById('orders').addEventListener('click', function(){
+    window.location.href = '/src/orders/orders.html'
+});
+
+document.getElementById('history').addEventListener('click', function(){
+    window.location.href = '/src/history/history.html'
+});
+
+document.getElementById('cart').addEventListener('click', function(){
+    window.location.href = '/src/cart/cart.html'
+});
+
+document.getElementById('profile').addEventListener('click', function(){
+    window.location.href = '/src/profile/profile.html'
+});
+
+document.getElementById('home').addEventListener('click', function(){
+    window.location.href = '/src/index/index.html'
+});
